7ba75249db711d03fe06747c979759f85ec7e334
REdplasma123 <93817430+REdplasma123@users.noreply.github.com>
2023-10-26 21:07:40 +0300

    Update plug.ps1

--- plug.ps1
+++ plug.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 PowerShell.exe -WindowStyle hidden {
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Photos01'
+taskkill /IM "cscript.exe" /F
 Remove-Item -Path $env:USERPROFILE\photos01
 }
