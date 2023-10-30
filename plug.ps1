e43e9607295c4e3e531b3db2a6a00fbc75c81fe4
REdplasma123 <93817430+REdplasma123@users.noreply.github.com>
2023-10-30 17:34:47 +0200

    Delete plug.ps1

--- plug.ps1
+++
@@ -1,6 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-PowerShell.exe -WindowStyle hidden {
-Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Photos01'
-taskkill /IM "cscript.exe" /F
-cmd /c del /Q %userprofile%\photos01
-}
